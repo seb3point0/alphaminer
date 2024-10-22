f40451749012962576eeb8253a8b35fc5c1806a5
Sebastien Couture <sebastien@interop.ventures>
2024-10-22 16:40:10 +0400

    implement telegram handler

--- app/main.py
+++ app/main.py
@@ -1,17 +1,22 @@
 import logging
+import asyncio
 from fastapi import FastAPI
 from app.core.config import settings
-from app.workers.tasks import sample_task
+from app.services.telegram import setup_bot
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-app = FastAPI(title=settings.PROJECT_NAME, debug=settings.DEBUG)
+project_name = settings.PROJECT_NAME
+
+app = FastAPI(title=project_name, debug=settings.DEBUG)
 
 @app.on_event("startup")
 def startup_event():
-    logger.info(f"Startup: {settings.PROJECT_NAME}")
+    logger.info(f"Startup: {project_name}")
+    asyncio.create_task(setup_bot(settings.TELEGRAM_TOKEN))
+
 
 @app.get("/health")
 async def health_check():
@@ -19,9 +24,9 @@ async def health_check():
 
 @app.get("/")
 def read_root():
-    return {"message": f"Welcome {settings.PROJECT_NAME}!"}
+    return {"message": f"Welcome {project_name}!"}
 
-@app.get("/task/{x}")
-def add(x: int):
-    task = sample_task.delay(x)
-    return {"task_id": task.id, "status": "Task submitted!"}
+# @app.get("/task/{x}")
+# def add(x: int):
+#     task = sample_task.delay(x)
+#     return {"task_id": task.id, "status": "Task submitted!"}
